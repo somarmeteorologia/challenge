57994b6dbd2613239f885e94feaf98f3b575b25d
Rafael Souza <rafaelangical2@gmail.com>
2019-05-29 00:12:07 -0300

    Refactor and resolved bug of lat and long undefined on request

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,46 +1,40 @@
 import React from 'react';
 import axios from 'axios';
 
-import { Redirect, Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 import Loading from '../components/Loading';
 import Graphic from '../components/Graphic';
 
-import { RootMain, Table, RowDate, RowTmax, RowTmin, RowHum, Tbody, Th, ThTop, Td, ContainerGraphic } from '../styles';
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config({path: ".env"});
-}
+import { RootMain, Table, RowDate, RowTmax, RowTmin, RowHum, Tbody, Th, ThHum, ThTop, Td, ContainerGraphic } from '../styles';
+import '../index.css';
 
 class Main extends React.Component {
   constructor(props) {
     super();
 
     this.state= {
-      latitude: '',
-      longitude: '',
       data: [],
       loaded: false,
       graphic: []
     }
   }
 
-  async requestData() {
+  async componentDidMount() {
+    
+    await navigator.geolocation.getCurrentPosition( location => {
+      const latitude = location.coords.latitude;
+      const longitude = location.coords.longitude; 
+      
+      const url = "https://nimbus.somar.io/forecast/10days?latitude=" + latitude + "&longitude=" + longitude + "&reference=Somar";
 
-    const url1 = `https://nimbus.somar.io/forecast/10days?latitude="${this.state.latitude}"&longitude="${this.state.longitude}"&reference=Somar"`;
-    const url = "https://nimbus.somar.io/forecast/10days?latitude=" + -23.33032 + "&longitude=" + -46.633308 + "&reference=Somar";
-
-    if( this.state.latitude !== undefined && this.state.longitude !== undefined){
-      await axios.get(url, {
+      axios.get(url, {
         headers: { 
           "x-api-key": process.env.REACT_APP_API_TOKEN,
         }})
         .then(resp => {
           // handle success
-          console.log(resp);
-          console.log(resp.data);
           this.setState({ data: resp.data , loaded: true });
-          console.log(this.state.data)
           this.makeGraphic(resp.data);
         })
         .catch(function (error) {
@@ -48,23 +42,9 @@ class Main extends React.Component {
           console.log('Error!!')
           console.log(error);
         })
-    }else{
-      alert('Latitude e longitude não autorizadas pelo browser ou invalidas'); 
-    }
-  }
-  async componentDidMount() {
-
-    await navigator.geolocation.getCurrentPosition( location => {
-      this.setState({ 
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude 
       });
-    });
-
-    await this.requestData();
     
   }
-
 
   formatTemp = temp => {
     const n_temp = temp.toString();
@@ -73,7 +53,6 @@ class Main extends React.Component {
   
   formatHum = hum => {
     const og = Math.pow(10, 0)
-    console.log(Math.floor(hum * og) / og);
     return Math.floor(hum * og) / og;
   };
 
@@ -91,7 +70,7 @@ class Main extends React.Component {
   makeGraphic = async data => {
     let graphic = [];
     const temp = new Date();
-    for (let i = 0; i < data.days.length - 3; i++) {
+    for (let i = 0; i < data.days.length - 2; i++) {
       if(this.formatData(data.days[i]) === temp.getDate()) {
         return graphic.push({
           day: this.formatData(data.days[i])
@@ -104,7 +83,6 @@ class Main extends React.Component {
         hum: this.formatHum(data.points.forecast.rel_humidity_daily_avg[i]) 
       });
     }
-    console.log(graphic);
     this.setState({ graphic });
   };
   
@@ -128,73 +106,73 @@ class Main extends React.Component {
               <RowTmax>
                 <Th>
                   {this.state.graphic.map((item,id) => {
-                    return <Td style={{color: '#E86C60'}} key={id}><i style={{marginRight: 10}} class="fas fa-caret-up"></i>{item.tmax}°C</Td>
+                    return <Td style={{color: '#E86C60'}} key={id}><i className="icon-t-max" class="fas fa-caret-up"></i>{item.tmax}°C</Td>
                   })}
                 </Th>
               </RowTmax>
               <RowTmin>
                 <Th>
                   {this.state.graphic.map((item,id) => {
-                    return <Td style={{color: '#39EBFF'}} key={id}><i style={{marginRight: 10}} class="fas fa-caret-down"></i>{item.tmin}°C</Td>
+                    return <Td style={{color: '#39EBFF'}} key={id}><i className="icon-t-min" class="fas fa-caret-down"></i>{item.tmin}°C</Td>
                   })}
                 </Th>
               </RowTmin>
               <RowHum>
-                <Th>
+                <ThHum>
                   {this.state.graphic.map((item,id) => {
                     return (
                       <Td>
                         {item.hum > 0 && item.hum <= 20 ? 
                           <>
-                            <span style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3 }}></span>
-                            <span style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px', marginRight: 3   }}></span>
-                            <span style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
-                            <span style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
-                            <span style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px'}}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3 }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px', marginRight: 3   }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px', marginRight: 5 }}></span>
                           </> 
                           : 
                           null
                         }
                         {item.hum > 20 && item.hum <= 40 ? 
                           <>
-                            <span style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3 }}></span>
-                            <span style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3   }}></span>
-                            <span style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
-                            <span style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
-                            <span style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px'}}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3 }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3   }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px', marginRight: 5 }}></span>
                           </> 
                           : 
                           null
                         }
                         {item.hum > 40 && item.hum <= 60 ? 
                           <>
-                            <span style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3 }}></span>
-                            <span style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3   }}></span>
-                            <span style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
-                            <span style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
-                            <span style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px'}}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3 }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3   }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px', marginRight: 5 }}></span>
                           </> 
                           : 
                           null
                         }
                         {item.hum > 60 && item.hum <= 80 ? 
                           <>
-                            <span style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3 }}></span>
-                            <span style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3   }}></span>
-                            <span style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
-                            <span style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
-                            <span style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px'}}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3 }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3   }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#C4C4C4', width: '3px', 'height': '13px', marginRight: 5 }}></span>
                           </> 
                           : 
                           null
                         }
                         {item.hum > 80 && item.hum <= 100 ? 
                           <>
-                            <span style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3 }}></span>
-                            <span style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3   }}></span>
-                            <span style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
-                            <span style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
-                            <span style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px'}}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3 }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 3   }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px',  marginRight: 3  }}></span>
+                            <span className="item-hum-label" style={{ backgroundColor: '#3FA2F7', width: '3px', 'height': '13px', marginRight: 5 }}></span>
                           </> 
                           : 
                           null
@@ -203,7 +181,7 @@ class Main extends React.Component {
                       </Td>
                       )
                   })}
-                </Th>
+                </ThHum>
               </RowHum>
             </Tbody>
           </Table>
