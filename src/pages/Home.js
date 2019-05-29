729fa35759738a6889df59857607a4ad0e9ee3d4
Rafael Souza <rafaelangical2@gmail.com>
2019-05-29 00:11:21 -0300

    Clean up and update style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,49 +1,25 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Link } from "react-router-dom";
-import moment from 'moment';
 import { Root, Title, Button, Subtitle } from '../styles';
 
-class Home extends React.Component {
-  state = {
-    latitude: '',
-    longitude: '',
-  }
-  async componentDidMount () {
-    await navigator.geolocation.getCurrentPosition( location => {
-      console.log(location);
-      this.setState({latitude: location.coords.latitude, longitude: location.coords.longitude});
-    });
-
-    //moment.defineLocale('pt-br', {
-    //  weekdays : 'Domingo_Segunda-feira_Terça-feira_Quarta-feira_Quinta-feira_Sexta-feira_Sábado'.split('_')
-    //});
-//
-    //console.log(moment('28-05-2019', 'DD-MM-YYYY').format('dddd'));
-    //console.log(moment().format());
-    console.log(new Date().getDay());
-  }
-  render() {
-    return (
-      <Root>
-        <img src={require('../assets/images/cloud-159394_960_720.png')} alt="image logo"  className="logo-image"/>
-        <Title>Minha Semana</Title>
-        <Subtitle>Previsões meteorologicas de acordo com sua localidade</Subtitle>
-        <Button>
-          <Link style={{color: '#FFF', textDecoration: 'none'}} 
-            to={{
-              pathname: '/main',
-              state: {
-                latitude: this.state.latitude,
-                longitude: this.state.longitude,
-                logged: true
-            }}}>
-            Avançar
-          </Link>
-        </Button>
-      </Root>
-    );
-  }
+const Home = () => {
+  return (
+    <Root>
+      <img src={require('../assets/images/cloud-159394_960_720.png')} alt="image"  className="logo-image"/>
+      <Title>Minha Semana</Title>
+      <Subtitle>Previsões meteorológicas de acordo com sua localidade</Subtitle>
+      <Button>
+        <Link style={{color: '#777', textDecoration: 'none'}} 
+          to={{
+            pathname: '/main',
+            state: {
+              logged: true
+          }}}>
+          Avançar
+        </Link>
+      </Button>
+    </Root>
+  );
 }
 
 export default Home;
