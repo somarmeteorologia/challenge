c6809018d49736791119675c41b48de97a1065cc
Rafael Souza <rafaelangical2@gmail.com>
2019-06-03 18:06:02 -0300

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  module: {
+    loaders: [
+      {exclude: ['node_modules'], loader: 'babel', test: /\.jsx?$/},
+      {loader: 'style-loader!css-loader', test: /\.css$/},
+      {loader: 'url-loader', test: /\.gif$/},
+      {loader: 'file-loader', test: /\.(ttf|eot|svg)$/},
+    ],
+  },
+  resolve: {
+    alias: {
+      config$: './configs/app-config.js',
+      react: './vendor/react-master',
+    },
+    extensions: ['', 'js', 'jsx'],
+    modules: [
+      'node_modules',
+      'bower_components',
+      'shared',
+      '/shared/vendor/modules',
+    ],
+  },
+};
