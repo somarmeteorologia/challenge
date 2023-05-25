e04772b490e2aac999a502f621a9032f48577691
matheusfc <matheuscorocher@gmail.com>
2023-05-25 15:08:08 -0300

    feat: add a sample of flask swagger

--- src/app.py
+++ src/app.py
@@ -1,15 +1,61 @@
 from flask import Flask, jsonify
+from flasgger import Swagger
+
 from pymongo import MongoClient
 
+
 app = Flask(__name__)
+swagger = Swagger(app)
 
 # Connect to MongoDB
 client = MongoClient("mongo", 27017)
 db = client["mydatabase"]
 collection = db["mycollection"]
 
+# Routes
 @app.route('/')
 def hello():
     return jsonify({
         'message': 'Hello, Flask and MongoDB!'
     })
+
+@app.route('/colors/<palette>/')
+def colors(palette):
+    """Example endpoint returning a list of colors by palette
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: palette
+        in: path
+        type: string
+        enum: ['all', 'rgb', 'cmyk']
+        required: true
+        default: all
+    definitions:
+      Palette:
+        type: object
+        properties:
+          palette_name:
+            type: array
+            items:
+              $ref: '#/definitions/Color'
+      Color:
+        type: string
+    responses:
+      200:
+        description: A list of colors (may be filtered by palette)
+        schema:
+          $ref: '#/definitions/Palette'
+        examples:
+          rgb: ['red', 'green', 'blue']
+    """
+    all_colors = {
+        'cmyk': ['cyan', 'magenta', 'yellow', 'black'],
+        'rgb': ['red', 'green', 'blue']
+    }
+    if palette == 'all':
+        result = all_colors
+    else:
+        result = {palette: all_colors.get(palette)}
+
+    return jsonify(result)
